876daec6dc6cc6627963a694acbf663cf4725f87
lpsantos <luanpatrick584@outlook.com>
2019-12-30 08:16:19 -0300

    Configure Nodemon and Sucrase-node

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  return res.json("Hello World");
+  return res.json("Hello Rocketseat");
 });
 
-module.exports = routes;
+export default routes;
